777507323b900c3fc3035e710c93d4fd8fedf96d
eugen <jeny797@gmail.com>
2021-03-24 23:52:14 +0200

    completed commit

--- sample_crawler/sample_crawler.py
+++ sample_crawler/sample_crawler.py
@@ -1,20 +1,16 @@
-from os import path
+from os import path, mkdir
 from time import time
 from io import BytesIO
 from re import match
 
 from cairosvg import svg2png
 from requests_html import HTMLSession
-from PIL import Image
-
-session = HTMLSession()
-start_time = time()
+from PIL import Image, UnidentifiedImageError
+from requests.exceptions import SSLError
 
 SAVING_DIR = 'output_dir'
-IMAGE_TYPE_LIST = {'jpeg', 'png', 'webp', 'bmp', 'dib', 'esp', 'iso', 'tga', 'tiff'}
+IMAGE_TYPE_LIST = {'jpeg', 'png', 'webp', 'bmp', 'dib', 'esp', 'iso', 'tga', 'tiff', 'svg', 'jpg'}
 
-start_url = 'https://requests-html.kennethreitz.org'
-# start_url = 'https://english-tutor-jeny797-2.herokuapp.com'
 count = 1
 image_saving_type = 'png'
 
@@ -23,56 +19,76 @@ visited_image_list = set()
 
 
 def search_image_worker(url):
-
     global count
-    r = session.get(url)
-    # print(url)
-    # print(r.status_code)
+    try:
+        r = session.get(url)
+    except SSLError:
+        return 'SSL access error'
+
     if r.status_code != 200:
-        return f'Staus code of {url} is {r.status_code}'
+        return f'Status code of {url} is {r.status_code}'
 
     page_links = set(r.html.absolute_links)
-    page_images = set([i.attrs['src'] for i in r.html.xpath('//img')])
-
     not_visited_links = page_links.difference(visited_links_list)
-    not_handled_images = page_images.difference(visited_image_list)
+    visited_links_list.update(not_visited_links)
 
-    for image_link in not_handled_images:
-        print(image_link)
-        image_link = image_link.split('?')[0]
-        visited_image_list.add(image_link)
+    temp_image_list = r.html.xpath('//img')
+    if len(temp_image_list) > 0:
+        page_images = set([i.attrs['src'] for i in temp_image_list])
+        not_handled_images = page_images.difference(visited_image_list)
+        print(f'not_handled_images {len(not_handled_images)}')
+        for image_link in not_handled_images:
+            visited_image_list.add(image_link)
+            image_link = image_link.strip()
 
-        # check the link is absolute or relative
-        if not match(r'^(?:[a-z]+:)?\/\/', image_link):
-            image_link = start_url + '/' + image_link
-        print(image_link)
+            # check the link is absolute or relative
+            if match(r'^(?:[a-z]+:)?\/\/', image_link) is None:
+                image_link = start_url + '/' + image_link
 
-        # check image type
-        download_image_type = image_link.split('.')[-1]
-        if download_image_type not in IMAGE_TYPE_LIST:
-            print(f"Image type {download_image_type} not suported.")
-            continue
+            try:
+                image = session.get(image_link)
+            except SSLError:
+                print('SSL access error')
+                continue
+            if image.status_code == 404:
+                print(f'{image_link} {image.status_code}')
+                continue
 
-        if download_image_type == 'svg':
-            print('SVG')
-            svg2png(url=image_link, write_to=path.join(SAVING_DIR,f'image-{count}.{image_saving_type}'))
-        image = session.get(image_link)
-        content_type = image.headers['Content-Type'].split('/')[-1]
+            # check image type
+            download_image_type = image_link.split('.')[-1]
+            if download_image_type == 'svg':
+                print('SVG')
+                svg2png(url=image_link, write_to=path.join(SAVING_DIR, f'image-{count}.{image_saving_type}'))
+            else:
+                try:
+                    with Image.open(BytesIO(image.content)).convert("RGB") as file:
+                        file.save(path.join(SAVING_DIR, f'image-{count}.{image_saving_type}'))
+                except UnidentifiedImageError:
+                    print(f"Can't handle image {image_link}")
+            count += 1
 
-        # with Image.open(requests.get(image_link, stream=True).raw) as file:
-        with Image.open(BytesIO(image.content)).convert("RGB") as file:
-            file.save(path.join(SAVING_DIR,f'image-{count}.{image_saving_type}'))
-        count +=1
+    print(f'not_visited_links {len(not_visited_links)}')
+    print("page count", len(visited_links_list))
 
     for page_link in not_visited_links:
+
         if match(start_url, page_link):
-            visited_links_list.add(page_link)
+            if page_link.split('/')[-1] == 'captcha':
+                continue
             search_image_worker(page_link)
 
-print(search_image_worker(start_url))
-print(len(visited_links_list))
-print(len(visited_image_list))
-print(visited_image_list)
-print((time() - start_time))
 
+if __name__ == '__main__':
+    session = HTMLSession()
+    start_time = time()
+    if not path.exists(SAVING_DIR):
+        mkdir(SAVING_DIR)
 
+    start_url = input('Insert site url for crawling ')
+    search_image_worker(start_url)
+
+    print('crawling process is complete')
+    print(f'Amount of visited links {len(visited_links_list)}')
+    print(f'Amount of visited images links {len(visited_image_list)}')
+
+    print(f'Work time is {(time() - start_time)}')
